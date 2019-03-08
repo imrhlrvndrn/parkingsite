0037351821e36931f8359b5f3fb82425fac8257a
Rahul Ravindran <rahul1116@users.noreply.github.com>
2019-03-08 21:34:32 +0530

    fix: Added the hidden navigation to the index.html

--- js/index.js
+++ js/index.js
@@ -1,13 +1,8 @@
 // variable for nav code
 let prevScrollpos = window.pageYOffset;
 
-// variables for the pricing charts
-const circle_link_id = document.getElementById("circle-link");
-const booking_info = document.getElementById('booking-info');
-const booking_form = document.getElementById('booking-form');
-const booking_info_h1 = document.querySelector('#booking-info-h1');
-const price_tables = document.getElementById('price_tables');
-const booking = document.getElementById('booking');
+// // variables for the pricing charts
+const closeIcon = document.getElementsByClassName("close");
 
 
 
@@ -25,44 +20,27 @@ window.onscroll = function() {
     // nav animation ends here
 }
 
-// This code makes the click-link button functional for showing the pricing charts
-// This entire code is for desktop version
-const pricing = () => {
-    // click_link.classList.add('pricing_animation');
-    booking_info.style.background = '#27233a';
-    booking_info.style.width = '100%';
-    booking_info.style.transition = '.7s';
-    document.getElementById('booking-info').firstElementChild.style.opacity = '1';
-    booking_info_h1.textContent = "Pricing";
-    booking_info_h1.style.color = "#eee";
-    booking_form.style.width = '0%';
-    booking_form.style.display = 'none';
-    circle_link_id.style.opacity = '0';
-    price_tables.style.display = 'grid';
-    booking.style.background = 'var(--dblue)';
+
+const close = () => {
+    closeIcon.parentNode.parentNode.style.opacity = "0";
+
 
     setTimeout(function(){
-        circle_link_id.style.display = 'none';      
-    },1000);
+        closeIcon.parentNode.parentNode.style.display = "none";
+    },500);
 }
 
-const booking_close = () => {
-    booking_info.style.background = '#fff';
-    booking_info.style.width = '60%';
-    booking_info.style.transition = '.7s';
-    document.getElementById('booking-info').firstElementChild.style.opacity = '0';
-    booking_info_h1.textContent = "Select a slot for your car";
-    booking_info_h1.style.color = "#222";
-    booking_form.style.width = '40%';
-    booking_form.style.display = 'flex';
-    circle_link_id.style.display = 'block';
-    circle_link_id.style.opacity = '1';
-    price_tables.style.display = 'none';
-    booking.style.background = '#fff';
+
+const hover = (x) => {
+    x.style.opacity = '1';
+    let siblings = getSiblings(x);
+    siblings.style.opacity = '.3';
 }
 
+const rmhover = (x) => {
+    x.style.opacity = '.5';
+}
 
 
 // All the eventListeners are addded here
-// click_link.addEventListener("click", pricing, false);
-document.getElementById('booking-info').firstElementChild.addEventListener("click", booking_close, false);
+
