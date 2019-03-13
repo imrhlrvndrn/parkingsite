90caaf88dd6d052a2d9c964b9ca0c446c63e6088
Rahul Ravindran <rahul1116@users.noreply.github.com>
2019-03-13 21:18:31 +0530

    Updated all

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@
 let prevScrollpos = window.pageYOffset;
 
 // // variables for the pricing charts
-const closeIcon = document.getElementsByClassName("close");
+const closeIcon = document.querySelectorAll(".close");
+const hidden_menu = document.getElementById("#hidden_menu");
+const nav = document.querySelector('nav');
 
 
 
@@ -21,26 +23,31 @@ window.onscroll = function() {
 }
 
 
-const close = () => {
-    closeIcon.parentNode.parentNode.style.opacity = "0";
-
+const navclose = () => {
+    hidden_menu.style.opacity = "0";
+    
 
     setTimeout(function(){
-        closeIcon.parentNode.parentNode.style.display = "none";
-    },500);
+        hidden_menu.style.display = "none";
+    },1000);
 }
 
 // The hover and rmhover functions are used for the hidden navigation menu
-const hover = (x) => {
-    x.style.opacity = '1';
-    let siblings = getSiblings(x);
-    siblings.style.opacity = '.3';
-}
+// const hover = (x) => {
+//     x.style.opacity = '1';
+//     let siblings = getSiblings(x);
+//     siblings.style.opacity = '.3';
+// }
 
-const rmhover = (x) => {
-    x.style.opacity = '.5';
-}
+// const rmhover = (x) => {
+//     x.style.opacity = '.5';
+// }
 
+const hiddennav = () => {
+    const hidden_menu = document.getElementById("#hidden_menu");
+    nav.style.display = "none";
+    document.getElementById("#hidden_menu").style.display = 'block';
+}
 
 // All the eventListeners are addded here
 
